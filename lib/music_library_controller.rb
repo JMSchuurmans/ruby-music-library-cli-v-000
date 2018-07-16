ab52ac8eac90b9458d742bb12be602879ead41c8
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-07-16 22:47:50 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -38,10 +38,7 @@ class MusicLibraryController
   end
 
   def list_songs
-    Song.all.sort_by { |song| song.name }
-      song.each.with_index(1) do |song, index|
-        puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-    end    #Song.all.sort_by(&:name).each.with_index(1) { |song, index| puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}" } #Prints all the songs in the music library in a numbered list.
+    Song.all.sort_by(&:name).each.with_index(1) { |song, index| puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}" } #Prints all the songs in the music library in a numbered list.
   end      #Song.all.sort_by(&:name) is the same as saying Song.all.sort_by{|song| song.name}. Lovely Ruby shorthand!
 
   def list_artists
